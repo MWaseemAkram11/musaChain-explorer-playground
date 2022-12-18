d50cb184893e342e3fb4fe93015eadd08d093f1b
MWaseemAkram11 <mwaseemakram335@gmail.com>
2022-12-18 22:57:57 +0500

    changes to banner section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,22 +1,39 @@
 import React from 'react'
-import bannerImg from "../../assets/images/bgg.jpg"
 import "./Banner.css";
-import icon from "../../assets/heart.png";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import etherStatGraph from "../../assets/images/ether-st-graph.PNG";
 import logo1 from "../../assets/images/logo1.svg";
 import logo2 from "../../assets/images/logo2.svg";
 import logo3 from "../../assets/images/logo3.svg";
 import logo4 from "../../assets/images/logo4.svg";
-
-
+// import searchIcon from "../../assets/images/search-icon.png";
 
 const Banner = ()=> {
   return (
     <div className='banner-container'>
-    <div>
-     
-      <img className='photo' src={`https://etherscan.io/images/svg/components/abstract-shapes-20.svg?v=1`} />
-    </div>
+      <div className='banner-img-section'>
+        <img  className='photo' src={`https://etherscan.io/images/svg/components/abstract-shapes-20.svg?v=1`} alt="banner-image" />
+      </div>
+      <div className='srach-section'>
+        <p className='search-p'>
+          The Musa Blockchain Explorer
+        </p>
+        <div>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="search by address"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2" className='search-btn'>
+              {/* <img alt="search-icon" src={searchIcon} style={{height:"40px", width:"100%"}} /> */}
+              Search
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
     <section className='price-stats'>
       <div className='left-stats'>
         <div className='left-main'>
